f1f6d526a6e2aa2b5fdfab83a94d7b6535ca7d44
cihatgul <ecihatgul@gmail.com>
2022-12-15 15:55:09 +0300

    first commit

--- src/D08_practice4/C04_MDA_ElementleriToplama.java
+++ src/D08_practice4/C04_MDA_ElementleriToplama.java
@@ -1,4 +1,16 @@
 package D08_practice4;
 
 public class C04_MDA_ElementleriToplama {
+    public static void main(String[] args) {
+
+        int[] arr1 ={3,5,7,8}; // 23
+
+        int toplam=0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            toplam=toplam+arr1[i];
+        }
+        System.out.println("Tek katli Arrays'in elementleri toplami: "+toplam); // 23
+
+    }
 }
